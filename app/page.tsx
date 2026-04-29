11cde8523d81391e7003e4f936f3868ef92db0d9
Mikhael Opeyemi-Olatunji <mikhael@Mikhaels-MacBook-Pro.local>
2026-04-29 11:43:37 -0700

    feat: about section with stack panel

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // app/page.tsx
 import Nav from '@/components/nav/Nav'
 import Hero from '@/components/hero/Hero'
+import About from '@/components/about/About'
 
 export default function Home() {
   return (
     <main>
       <Nav />
       <Hero />
+      <About />
     </main>
   )
 }
